443f4733f56563301e3a631328894a43691cabfe
shakuov_nursultan <priestnurs7@gmail.com>
2022-07-13 15:56:32 +0600

    сделал 3-ю задачку

---
+++ homeworks/3-hometask/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2){
+  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
