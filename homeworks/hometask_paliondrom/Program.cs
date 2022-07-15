8905f8a46ad83b9bf916b7a915d79ecec46e8f51
shakuov_nursultan <priestnurs7@gmail.com>
2022-07-15 14:27:00 +0600

    сделал задачку на определение палиондрома

---
+++ homeworks/hometask_paliondrom/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("определить является ли число палиондромом");
+Console.Write("Введите число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number){
+  if (number[0]==number[4] || number[1]==number[3]){
+    Console.WriteLine($"Ваше число: {number} - палиндром.");
+  }
+  else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
+}
+
+if (number!.Length == 5){
+  CheckingNumber(number);
+}
+else Console.WriteLine($"Введи правильное число");
